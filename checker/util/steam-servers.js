dbfbde408e998db80ce13071a31f73d80e9fbe02
Francisco Castro <fjcastrocs@gmail.com>
2019-01-28 13:58:31 -0500

    uneed folder

--- checker/util/steam-servers.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const axiosRetry = require("axios-retry")
-
-module.exports = GetSteamServers = () => {
-    axiosRetry(axios, { retries: 5, retryDelay: () => { return 2000;} });
-    let url = "https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?format=json&cellid=0";
-    console.log("Fetched STEAM CM Servers.")
-    return axios.get(url);
-}
