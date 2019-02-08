45d498f02393193500273abef0f7f4a4fcb8f23e
Francisco Castro <fjcastrocs@gmail.com>
2019-02-07 23:14:27 -0500

    remove uneeded folder

--- checker/test.js
+++
@@ -1,8 +0,0 @@
-process.env.testMode = true;
-
-let Checker = require("./index");
-
-let combos = [];
-combos[0] = "laxin4life23@hotmail.com:bigpimp1" //good hit unverified
-
-let checker = new Checker(combos);
--- checker/util/get-bad-ids.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const axiosRetry = require("axios-retry")
-
-module.exports = GetBadSteamIds = (digit) => {
-    axiosRetry(axios, { retries: 5, retryDelay: () => { return 2000;} });
-
-    let url = `http://gdl.site.nfoservers.com/bad${digit}dig.txt`;
-    return axios.get(url);
-};
