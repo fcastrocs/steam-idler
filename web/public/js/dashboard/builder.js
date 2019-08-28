249d86c85ae353580ac78bdcd31b7f99ca0f3e42
Francisco Castro <fcastro16@gmail.com>
2019-08-27 21:28:53 -0400

    removed spinner animation

--- web/public/js/dashboard/builder.js
+++ web/public/js/dashboard/builder.js
@@ -1,24 +1,26 @@
+// accounts builder and refresher
 $(async () => {
-    let accountsHtml = ""
+    $("#body-spinner").hide()
     let accounts = await FetchAllAccounts();
     if (accounts.length == 0) {
-        $("#body").css("display", "unset")
-        accountsHtml = `
-            <h2>No steam accounts associated with your account.</h2>
-            <button type="button" id="big-add-btn" class="btn header-btn" data-toggle="modal" data-target="#add-acc-modal">
-                Add Account
-            </button>`
+        $("#body-spinner").hide()
+        $("#body").css("display", "unset");
+        $("#no-accounts-box").prop("hidden", false)
     } else {
+        let fade = 0
         for (let i in accounts) {
             // save accounts in cache
             accounts_cache[accounts[i]._id] = accounts[i];
             // build account
-            accountsHtml += buildAccount(accounts[i])
+            $("#accounts-box").append(buildAccount(accounts[i]))
+            let self = $(`div[data-id="${accounts[i]._id}"]`);
+            self.prop("hidden", true);
+            setTimeout(() => {
+                self.prop("hidden", false);
+            }, fade);
+            fade += 50
         }
     }
-
-    $("#body-spinner").hide()
-    $("#accounts-box").html(accountsHtml)
 
     // Refresh dashboard every 30 seconds
     let interval = 1 * 60 * 1000
