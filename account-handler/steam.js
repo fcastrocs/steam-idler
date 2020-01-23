5af50e55c6390cfbae0c4b3fc1af03f406c1522c
Francisco Castro <fjcastrocs@gmail.com>
2020-01-23 16:14:24 -0500

    converted item amount to integer

--- account-handler/steam.js
+++ account-handler/steam.js
@@ -308,7 +308,7 @@ module.exports.buildTradeOffer = function (inventory) {
     for (const item of inventory) {
         count++
 
-        if(count == 10){
+        if(count == 40){
             break;
         }
 
