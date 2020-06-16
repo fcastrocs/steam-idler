340e7e8e909d4e015e07b0491626c05450fb7499
Francisco Castro <fjcastrocs@gmail.com>
2020-06-16 17:08:12 -0400

    better proxies

--- util/proxy.js
+++ util/proxy.js
@@ -3,7 +3,7 @@ const Proxy = require('../models/proxy');
 
 // Returns proxy list from proxyscrape.com
 async function GetProxies() {
-    let url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=10000&country=ID"
+    let url = "http://account.fineproxy.org/api/getproxy/?format=txt&type=socksip&login=US342134&password=lVcjTDtpLJ"
 
     let proxyList = null;
     try {
